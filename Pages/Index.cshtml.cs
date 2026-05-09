66530bb3745882c646454e4ef667da407fe05cd9
edson.ruano <edsonruano1996@gmail.com>
2026-05-09 14:32:08 -0600

    Blue Deploy

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlueGreenDeploy.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
